e368bf226bfc4566872c633e83854439e829f599
mohdazeem0 <mohdazm007@gmail.com>
2021-10-09 21:01:17 +0530

    update

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,54 @@
 "use strict";
-let id = 5;
-console.log("ID:", id);
+//Basics Types
+let id = 50;
+let company = 'Mohd Azeem';
+let isPublished = true;
+let x = 'Hello There'; //any types of data types
+let ids = [1, 2, 3, 4, 5, 6];
+let arr = [1, true, 'hello']; //any types of data types
+//Tuple
+let person = [1, 'azeem', true]; //all should be in same spot as data types
+//Tuple Array
+let employee;
+employee = [
+    [1, 'azeem'],
+    [2, 'zeem'],
+    [3, 'azim'],
+];
+//Unions
+let prodId;
+prodId = 22;
+//Enums / Numerated Types//allows us to define set of name constants either numeric or strngs
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Up);
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "up";
+    Direction2["Down"] = "down";
+    Direction2["Left"] = "left";
+    Direction2["Right"] = "right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2.Down);
+const user = {
+    id: 1,
+    name: 'azeem'
+};
+//Type Assertion
+let cid = 1;
+//let customerId = <number>cid
+let customerId = cid;
+//Functions
+function addNum(x, y) {
+    return x + y;
+}
+console.log(addNum(2, 7));
+//Void
+function log(message) {
+    console.log(message);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,82 @@
 //Basics Types
 
-let id:number=50;
-let company : string ='Mohd Azeem';
-let isPublished:boolean=true;
-let x:any='Hello There'; //any types of data types
+let id: number = 50;
+let company: string = 'Mohd Azeem';
+let isPublished: boolean = true;
+let x: any = 'Hello There'; //any types of data types
 
 
-let ids:number[]=[1,2,3,4,5,6];
-let arr:any[]=[1,true,'hello'] //any types of data types
+let ids: number[] = [1, 2, 3, 4, 5, 6];
+let arr: any[] = [1, true, 'hello'] //any types of data types
 
 //Tuple
-let person:[number,string,boolean]=[1,'azeem',true] //all should be in same spot as data types
+let person: [number, string, boolean] = [1, 'azeem', true] //all should be in same spot as data types
 //Tuple Array
-let employee:[number, string][]
+let employee: [number, string][]
 
-employee=[
-    [1,'azeem'],
-    [2,'zeem'],
-    [3,'azim'],
+employee = [
+    [1, 'azeem'],
+    [2, 'zeem'],
+    [3, 'azim'],
 ]
 
 //Unions
-let prodId:string|number
-prodId=22;
+let prodId: string | number
+prodId = 22;
 
 //Enums / Numerated Types//allows us to define set of name constants either numeric or strngs
-enum Direction1{
+enum Direction1 {
     Up = 1,
     Down,
     Left,
     Right,
 }
 console.log(Direction1.Up);
+enum Direction2 {
+    Up = 'up',
+    Down='down',
+    Left='left',
+    Right='right',
+}
+console.log(Direction2.Down);
 
 
+//OBJECTS
+type User={
+    id:number,
+    name:string
+}
+const user:User={
+    id:1,
+    name:'azeem'
+    }
+
+//Type Assertion
+let cid:any=1
+//let customerId = <number>cid
+let customerId=cid as number
+
+//Functions
+function addNum(x:number,y:number):number{
+    return x+y
+}
+console.log(addNum(2,7));
+
+
+//Void
+function log(message:string|number):void{
+    console.log(message);
+}
+
+//Interfaces /custom types
+interface UserInterface{
+    id:number,
+    name:string
+}
+const user1:UserInterface={
+    id:1,
+    name:'azeem'
+    }
+ 
 
 
